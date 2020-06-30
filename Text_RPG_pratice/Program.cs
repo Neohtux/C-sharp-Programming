b8210d3be093f48fb8a67cce03f5abd7f42b7939
neohtux <neohtux@daum.net>
2020-06-30 12:56:44 +0900

    C# language pratice

---
+++ Text_RPG_pratice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+/*using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+namespace Csharp
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            game g = new game();
+
+            while(true)
+            {
+                g.Process();
+            }
+        }
+    }
+
+}
+
